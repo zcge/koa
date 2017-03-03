45633eea8f099cbb1d342cd58c8836d1777113c7
韩柯炜 <wb-hankewei@alibaba-inc.com>
2017-03-03 10:00:39 +0800

    密码

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ var config = {
     mysql: {
         database: 'hkw', // 使用哪个数据库
         username: 'root', // 用户名
-        password: '', // 口令
+        password: 'mysql123456', // 口令
         host: 'hankewei.xyz', // 主机名
         port: 3306 // 端口号，MySQL默认3306
     },
